b5f6412f42272c27fa197ee8aab6f9a90dc3d5a2
githiwi <hiwi5555@gmail.com>
2024-03-03 02:08:53 +0100

    scale graph background and css

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     calculateBtn.addEventListener('click', () => {
         layer1.style.display = 'none';
         layer2.style.display = 'block';
+        
         const chosenIncome = parseInt(incomeSlider.value);
         console.log("chosenIncome", chosenIncome);
 
